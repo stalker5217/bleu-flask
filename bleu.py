8f115e20f5855e66e33cfb3483751e48d4a8de0b
Song <stalker5217@gmail.com>
2021-01-27 13:13:01 +0900

    first commit

---
+++ bleu.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+import json
+import logging
+
+import nltk.translate.bleu_score as bleu
+
+app = Flask(__name__)
+
+@app.route("/score", methods=['GET'])
+def score():
+	candidate = request.args["candidate"]
+	references = request.args.getlist("references")
+
+	logging.info(references)
+
+	# NLTK 패키지 구현되어져 있는 코드로 계산한 BLEU 점수
+	score = bleu.sentence_bleu(list(map(lambda ref: ref.split(), references)), candidate.split())
+	ret = {}
+	ret['score'] = score
+
+	return json.dumps(ret)
+
+if __name__ == "__main__":
+	logging.getLogger().setLevel(logging.INFO)
+	app.run(debug=True)
